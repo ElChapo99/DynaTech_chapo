e10b4c919683f23188c0dbb2a01603456b52dc34
ElChapo99 <jesusduran2504@outlook.com>
2025-12-23 10:00:58 -0700

    Update TypedKey.java

--- src/main/java/me/profelements/dynatech/registries/TypedKey.java
+++ src/main/java/me/profelements/dynatech/registries/TypedKey.java
@@ -6,17 +6,17 @@ import javax.annotation.Nonnull;
 
 import org.bukkit.NamespacedKey;
 
-public record TypedKey<T>(@Nonnull NamespacedKey key) {
-    public static <T> TypedKey<T> create(NamespacedKey key) {
-        return new TypedKey<>(key);
+public record TypedKey<T>(@Nonnull NamespacedKey clave) {
+    public static <T> TypedKey<T> crear(NamespacedKey clave) {
+        return new TypedKey<>(clave);
     }
 
-    public static <T> TypedKey<T> create(String namespace, String key) {
-        return new TypedKey<>(new NamespacedKey(namespace, key));
+    public static <T> TypedKey<T> crear(String espacioDeNombres, String clave) {
+        return new TypedKey<>(new NamespacedKey(espacioDeNombres, clave));
     }
 
-    // THIS IS TEMPORARY TILL SLIMEFUN MOVES TO NamespacedKey
-    public String asSlimefunId() {
-        return this.key().toString().replace(':', '_').toUpperCase(Locale.ROOT);
+    // ESTO ES TEMPORAL HASTA QUE SLIMEFUN USE NamespacedKey
+    public String comoIdSlimefun() {
+        return this.clave().toString().replace(':', '_').toUpperCase(Locale.ROOT);
     }
 }
