a8558c5d2d77fe12843703f4b7932610e279d897
ElChapo99 <jesusduran2504@outlook.com>
2025-12-23 09:51:37 -0700

    Update Recipe.java

--- src/main/java/me/profelements/dynatech/utils/Recipe.java
+++ src/main/java/me/profelements/dynatech/utils/Recipe.java
@@ -10,96 +10,105 @@ import me.profelements.dynatech.registries.Registries;
 import me.profelements.dynatech.registries.TypedKey;
 
 public class Recipe {
-    private NamespacedKey KEY;
-    private RecipeType TYPE;
-    private ItemStack[] INPUT;
-    private ItemStack[] OUTPUT;
+    // Clave única de la receta
+    private NamespacedKey CLAVE;
+    
+    // Tipo de receta (Smeltery, Crafting, etc)
+    private RecipeType TIPO;
+    
+    // Ítems de entrada
+    private ItemStack[] ENTRADA;
+    
+    // Ítems de salida
+    private ItemStack[] SALIDA;
 
     protected Recipe() {
     }
 
+    // Inicializa la receta
     public static Recipe init() {
         return new Recipe();
     }
 
     public Recipe setKey(NamespacedKey key) {
-        Preconditions.checkNotNull(key, "The recipe's namespaced key should not be null");
-        this.KEY = key;
+        Preconditions.checkNotNull(key, "La clave namespaced de la receta no puede ser nula");
+        this.CLAVE = key;
         return this;
     }
 
     public final NamespacedKey getKey() {
-        return this.KEY;
+        return this.CLAVE;
     }
 
     public Recipe setRecipeType(RecipeType type) {
-        Preconditions.checkNotNull(type, "The recipe's type should not be null");
-        this.TYPE = type;
+        Preconditions.checkNotNull(type, "El tipo de receta no puede ser nulo");
+        this.TIPO = type;
         return this;
     }
 
     public final RecipeType getRecipeType() {
-        return this.TYPE;
+        return this.TIPO;
     }
 
     public Recipe setInput(ItemStack input) {
-        Preconditions.checkNotNull(input, "This recipe's inputs should not be null");
-        this.INPUT = new ItemStack[] { input };
+        Preconditions.checkNotNull(input, "Los ítems de entrada de la receta no pueden ser nulos");
+        this.ENTRADA = new ItemStack[] { input };
         return this;
     }
 
     public Recipe setInput(ItemStack[] input) {
-        Preconditions.checkNotNull(input, "This recipe's inputs should not be null");
-        this.INPUT = input;
+        Preconditions.checkNotNull(input, "Los ítems de entrada de la receta no pueden ser nulos");
+        this.ENTRADA = input;
         return this;
     }
 
     public final ItemStack[] getInput() {
-        return this.INPUT;
+        return this.ENTRADA;
     }
 
     public Recipe setOutput(ItemStack output) {
-        Preconditions.checkNotNull(output, "This recipe's output should not be null");
-        this.OUTPUT = new ItemStack[] { output };
+        Preconditions.checkNotNull(output, "El ítem de salida de la receta no puede ser nulo");
+        this.SALIDA = new ItemStack[] { output };
         return this;
     }
 
     public Recipe setOutput(ItemStack output, int amount) {
-        Preconditions.checkNotNull(output, "This recipe's output should not be null");
+        Preconditions.checkNotNull(output, "El ítem de salida de la receta no puede ser nulo");
         var actualOutput = output.clone();
         actualOutput.setAmount(amount);
-        this.OUTPUT = new ItemStack[] { actualOutput };
+        this.SALIDA = new ItemStack[] { actualOutput };
         return this;
     }
 
     public Recipe setOutput(ItemStack[] output) {
-        Preconditions.checkNotNull(output, "This recipe's output should not be null");
-        this.OUTPUT = output;
+        Preconditions.checkNotNull(output, "Los ítems de salida de la receta no pueden ser nulos");
+        this.SALIDA = output;
         return this;
     }
 
     public final ItemStack[] getOutput() {
-        return this.OUTPUT;
+        return this.SALIDA;
     }
 
+    // Valida que todos los campos estén correctamente inicializados
     public void validate() {
-        Preconditions.checkNotNull(this.KEY, "The recipe's namespaced key should not be null");
-        Preconditions.checkNotNull(this.TYPE, "The recipe's type should not be null");
-        Preconditions.checkNotNull(this.INPUT, "This recipe's inputs should not be null");
-        Preconditions.checkNotNull(this.OUTPUT, "This recipe's output should not be null");
-
+        Preconditions.checkNotNull(this.CLAVE, "La clave namespaced de la receta no puede ser nula");
+        Preconditions.checkNotNull(this.TIPO, "El tipo de receta no puede ser nulo");
+        Preconditions.checkNotNull(this.ENTRADA, "Los ítems de entrada de la receta no pueden ser nulos");
+        Preconditions.checkNotNull(this.SALIDA, "Los ítems de salida de la receta no pueden ser nulos");
     }
 
+    // Construye la receta y valida sus campos
     public Recipe build() {
         this.validate();
         return this;
     }
 
+    // Registra la receta en el sistema
     public Recipe register() {
         Recipe res = this.build();
         res.getRecipeType().register(res.getInput(), res.getOutput()[0]);
         Registries.RECIPES.register(TypedKey.create(res.getKey()), res);
         return res;
     }
-
 }
